0bbfe08cc37c996fcbfd4a08f06bf8459b9e6bef
Martinguyen <Mmn1134136@gmail.com>
2025-06-21 03:10:26 -0400

    Menu Dropbox under crimes in mapsettings.tsx

--- src/components/MapSettings.tsx
+++ src/components/MapSettings.tsx
@@ -1,58 +1,86 @@
 "use client";
 import React, { useState } from "react";
-import styles from "../app/page.module.css"; // or "../styles/sidebar.module.css"
+import styles from "../app/page.module.css";
 import { IoSettingsSharp } from "react-icons/io5";
 import Image from "next/image";
 import NorthStarIcon from "@/public/NorthStarIcon.svg";
 
 export function MapSettingsSidebar() {
-    const [open, setOpen] = useState<boolean>(false);
+  const [open, setOpen] = useState<boolean>(false);
+  const [showCrimeHeatmap, setShowCrimeHeatmap] = useState<boolean>(false);
 
-    return (
-        <>
-            <button
-                className={`${styles.sidebarToggle} ${open ? styles.sidebarToggleOpen : ""}`}
-                onClick={() => setOpen(o => !o)}
-            >
-                <Image src={NorthStarIcon} alt="Settings" width={32} height={32}/>
-                <span>Settings</span>
-            </button>
+  return (
+    <>
+      <button
+        className={`${styles.sidebarToggle} ${open ? styles.sidebarToggleOpen : ""}`}
+        onClick={() => setOpen((o) => !o)}
+      >
+        <Image src={NorthStarIcon} alt="Settings" width={32} height={32} />
+        <span>Settings</span>
+      </button>
 
-            <aside
-                className={`${styles.sidebar} ${open ? styles.sidebarOpen : ""}`}
-            >
+      <aside className={`${styles.sidebar} ${open ? styles.sidebarOpen : ""}`}>
+        <h2>Map Settings</h2>
 
-                <h2>Map Settings Working on it </h2>
-                {/*TODO add controls*/}
-                <div>
-                    <label>
-                        <input type="checkbox"/> Show crime heatmap
-                    </label>
-                    <div style={{ marginLeft: "24px", marginTop: "4px" }}>
-                        <label>
-                            <input type="checkbox" /> Walk: 300m
-                        </label>
-                        <br />
-                        <label>
-                            <input type="checkbox" /> Bike: 500m
-                        </label>
-                        <br />
-                        <label>
-                            <input type="checkbox" /> Car: 1000m
-                        </label>
-                    </div>
-                </div>
-                <div>
-                    <label>
-                        <input type="checkbox"/> Night mode weighting
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <input type="checkbox"/> Satellite map
-                    </label>
-                </div>
-            </aside>
-        </>
-    );
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={showCrimeHeatmap}
+              onChange={(e) => setShowCrimeHeatmap(e.target.checked)}
+            />{" "}
+            Show Crime Heatmap
+          </label>
+
+          {showCrimeHeatmap && (
+            <div style={{ marginLeft: "24px", marginTop: "4px" }}>
+              <label>
+                <input type="checkbox" /> Shootings
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Homicides
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Assaults
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Auto Thefts
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Bicycle Thefts
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Robberies
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Open data Thefts
+              </label>
+              <br />
+              <label>
+                <input type="checkbox" /> Motor Vehicle Thefts
+              </label>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" /> Night Mode Weighting
+          </label>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" /> Satellite Map
+          </label>
+        </div>
+      </aside>
+    </>
+  );
 }
