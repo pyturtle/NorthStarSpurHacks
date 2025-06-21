def121babdad8d6bcdaa09a5c96b64ff443483b2
Martinguyen <Mmn1134136@gmail.com>
2025-06-21 03:23:59 -0400

    Arrow Change

--- src/components/MapSettings.tsx
+++ src/components/MapSettings.tsx
@@ -2,12 +2,13 @@
 import React, { useState } from "react";
 import styles from "../app/page.module.css";
 import { IoSettingsSharp } from "react-icons/io5";
+import { FiChevronRight, FiChevronDown } from "react-icons/fi";
 import Image from "next/image";
 import NorthStarIcon from "@/public/NorthStarIcon.svg";
 
 export function MapSettingsSidebar() {
-  const [open, setOpen] = useState<boolean>(false);
-  const [showCrimeHeatmap, setShowCrimeHeatmap] = useState<boolean>(false);
+  const [open, setOpen] = useState(false);
+  const [showCrimeOptions, setShowCrimeOptions] = useState(false);
 
   return (
     <>
@@ -22,52 +23,57 @@ export function MapSettingsSidebar() {
       <aside className={`${styles.sidebar} ${open ? styles.sidebarOpen : ""}`}>
         <h2>Map Settings</h2>
 
-        <div>
-          <label>
-            <input
-              type="checkbox"
-              checked={showCrimeHeatmap}
-              onChange={(e) => setShowCrimeHeatmap(e.target.checked)}
-            />{" "}
-            Show Crime Heatmap
-          </label>
-
-          {showCrimeHeatmap && (
-            <div style={{ marginLeft: "24px", marginTop: "4px" }}>
-              <label>
-                <input type="checkbox" /> Shootings
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Homicides
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Assaults
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Auto Thefts
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Bicycle Thefts
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Robberies
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Open data Thefts
-              </label>
-              <br />
-              <label>
-                <input type="checkbox" /> Motor Vehicle Thefts
-              </label>
-            </div>
-          )}
+        {/* Arrow-based expandable crime heatmap section */}
+        <div
+          onClick={() => setShowCrimeOptions((prev) => !prev)}
+          style={{
+            cursor: "pointer",
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+            fontWeight: 600,
+            marginBottom: "4px",
+          }}
+        >
+          {showCrimeOptions ? <FiChevronDown size={18} /> : <FiChevronRight size={18} />}
+          <span>Show Crime Heatmap</span>
         </div>
+
+        {showCrimeOptions && (
+          <div style={{ marginLeft: "24px", marginTop: "4px" }}>
+            <label>
+              <input type="checkbox" /> Shootings
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Homicides
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Assaults
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Auto Thefts
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Bicycle Thefts
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Robberies
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Open Data Thefts
+            </label>
+            <br />
+            <label>
+              <input type="checkbox" /> Motor Vehicle Thefts
+            </label>
+          </div>
+        )}
 
         <div>
           <label>
