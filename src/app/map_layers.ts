a8296d45e61fd828fd1b7f7c3311610e12b65d64
Jay Mehta <jaybharatmehta2007@gmail.com>
2025-06-21 14:59:52 -0400

    added all data layers and renamed to sasha's naming convention

--- src/app/map_layers.ts
+++ src/app/map_layers.ts
@@ -1,13 +1,20 @@
 import mapboxgl from "mapbox-gl";
 
-// Import your datasets
+// Import datasets from public folder
 import shootings from "@/public/layer-data/shootings_2023-2025.json";
-// import assaults from "@/public/layer-data/assaults_2023-2025.json"; // example: another dataset
+import assaults from "@/public/layer-data/assaults.json";
+import autoThefts from "@/public/layer-data/auto thefts.json";
+import bicycleThefts from "@/public/layer-data/bicycle thefts.json";
+import homicides from "@/public/layer-data/homicides.json";
+import motorThefts from "@/public/layer-data/motor thefts.json";
+import robberies from "@/public/layer-data/robberies.json";
+import theftsOver from "@/public/layer-data/thefts over open.json";
 
 export class MapLayers {
-  static circleColor: string = "#ff0000"
-  static circleOpacity: number = 0.7
+  static circleColor: string = "#ff0000";
+  static circleOpacity: number = 0.7;
 
+  // Common paint style for all circle layers
   private static get commonPaint(): mapboxgl.CircleLayerSpecification['paint'] {
     return {
       "circle-radius": 8,
@@ -17,6 +24,7 @@ export class MapLayers {
     };
   }
 
+  // Add a generic layer to the map given ID and GeoJSON data
   private static addLayer(
     map: mapboxgl.Map,
     id: string,
@@ -39,6 +47,7 @@ export class MapLayers {
     }
   }
 
+  // Remove layer and source from the map
   private static removeLayer(map: mapboxgl.Map, id: string) {
     if (map.getLayer(id)) {
       map.removeLayer(id);
@@ -47,24 +56,60 @@ export class MapLayers {
       map.removeSource(id);
     }
   }
-  
-  // refreshing method
+
+  // Add all available datasets to the map
   static restoreAllLayers(map: mapboxgl.Map) {
     this.addShootings(map);
-    // this.addAssaults(map);
+    this.addAssaults(map);
+    this.addAutoThefts(map);
+    this.addBicycleThefts(map);
+    this.addHomicides(map);
+    this.addMotorThefts(map);
+    this.addRobberies(map);
+    this.addTheftsOver(map);
   }
 
   // Red lines appear on these methods due to typescript not understanding their type
   // its fine
+
   static addShootings(map: mapboxgl.Map) {
+    this.circleColor = "#ff0000"; // Red
+    this.circleOpacity = 1;
     this.addLayer(map, "shootings", shootings);
-    this.circleColor = "#0000ff"
-    this.circleOpacity = 1
   }
 
   static addAssaults(map: mapboxgl.Map) {
+    this.circleColor = "#e67300"; // Orange
     this.addLayer(map, "assaults", assaults);
-    this.circleColor = "#0000ff"
   }
 
+  static addAutoThefts(map: mapboxgl.Map) {
+    this.circleColor = "#e600e6"; // Pink
+    this.addLayer(map, "auto_thefts", autoThefts);
+  }
+
+  static addBicycleThefts(map: mapboxgl.Map) {
+    this.circleColor = "#4dc3ff"; // Light Blue
+    this.addLayer(map, "bicycle_thefts", bicycleThefts);
+  }
+
+  static addHomicides(map: mapboxgl.Map) {
+    this.circleColor = "#000000"; // Black
+    this.addLayer(map, "homicides", homicides);
+  }
+
+  static addMotorThefts(map: mapboxgl.Map) {
+    this.circleColor = "#9900cc"; // Purple
+    this.addLayer(map, "motor_thefts", motorThefts);
+  }
+
+  static addRobberies(map: mapboxgl.Map) {
+    this.circleColor = "#006600"; // Green
+    this.addLayer(map, "robberies", robberies);
+  }
+
+  static addTheftsOver(map: mapboxgl.Map) {
+    this.circleColor = "#999900"; // Yellow
+    this.addLayer(map, "thefts_over_open", theftsOver);
+  }
 }
